80b36961aafbe37605989f8271e184fdcf8ef697
Matias Kinnunen <github@mtsknn.fi>
2023-03-14 19:00:06 +0200

    Exercise 3: update Guesses component

--- src/components/Guesses/Guesses.js
+++ src/components/Guesses/Guesses.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
+import { range } from '../../utils';
 
 function Guesses({ guesses }) {
   return (
     <div className="guess-results">
-      {guesses.map((guess, i) => (
-        <p className="guess" key={i}>
-          {guess}
-        </p>
+      {range(0, NUM_OF_GUESSES_ALLOWED).map((i) => (
+        <Guess key={i} word={guesses[i] || ''} />
       ))}
     </div>
   );
 }
 
 export default Guesses;
+
+function Guess({ word }) {
+  return (
+    <p className="guess">
+      {range(0, 5).map((i) => (
+        <span className="cell" key={i}>
+          {word[i]}
+        </span>
+      ))}
+    </p>
+  );
+}
